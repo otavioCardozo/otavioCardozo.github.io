6fe305f67863c7125c64a9d9c10a28f77605af8f
wellyngton-souza <wellyngtonteixeira12@gmail.com>
2023-07-02 21:47:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function BuscarHora() {
+  var data = new Date();
+  var hora = data.getHours();
+
+  if (hora >= 6 && hora < 12) {
+      return "Good Morning";
+  } else if (hora >= 12 && hora < 18) {
+      return "Good Afternoon";
+  } else {
+      return "Good Evening";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  document.querySelector("h2").innerText = BuscarHora() + " Otavio";
+});
